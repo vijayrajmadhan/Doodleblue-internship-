07546a7e384c03ffc3c15229f79fbe7123962980
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-13 21:27:31 +0530

    Create Test.cs

---
+++ Class and object/Test.cs
@@ -0,0 +1,25 @@
+using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+
+namespace HelloWold;
+
+
+
+public class Test 
+
+{         
+
+     public void display()
+
+	 {
+
+	 	Console.WriteLine("hello");
+
+	 }
+
+}
