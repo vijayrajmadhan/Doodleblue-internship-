32a58710167e0576e6428a6d9159a01e77e29b37
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-13 21:26:47 +0530

    Create Program.cs

---
+++ Class and object/Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+
+namespace HelloWold;
+
+
+
+public static class Program 
+
+{         
+
+    static void Main(String[] args)
+
+    {
+
+		Test obj=new Test();
+
+		obj.display();
+
+        Console.WriteLine("Hello World!");
+
+    }
+
+}
+
+
+
+/* if you need to use value in class
+
+  outside main funcion you need to 
+
+  creat the object  */
