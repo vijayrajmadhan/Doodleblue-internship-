1edeaa6d57cc9a8dcc0c7e59de6016c18c1bddc3
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-14 10:34:40 +0530

    Create Food.cs
    The constructor is a special method that is used to initialize object.
    Advantages-it is called when an object of a class is created

---
+++ Constructor/Food.cs
@@ -0,0 +1,42 @@
+using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+
+namespace Constructor;
+
+
+
+public class Food 
+
+{         
+
+       public String model;
+       public String type;
+	   
+
+	  public Food(String modelName)//can add as many parameter you want
+
+	  {
+      type=modelName;
+	    model="banana";
+
+	  }
+
+	  
+
+	  static void Main(String[] args)
+
+	  {
+
+	     Food myobj=new Food("fruit");//it will set intial values to model
+
+		 Console.WriteLine(myobj.model);
+     Console.WriteLine(myobj.type);
+
+	  }
+
+}
