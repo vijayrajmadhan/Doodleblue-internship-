6f348a44ca6fa27ec807b0a6c519e7375404ac35
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-14 11:18:13 +0530

    Create Private.cs
    The code is only accessible within the same class

---
+++ Access Modifiers/Private.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharpShell;
+
+public class Private
+{         
+        private String model="Data";
+		
+		static void Main(string[] args)
+		{
+			 Private obj=new Private();
+			Console.WriteLine(obj.model);
+		}
+}
+
+/* if you try to access model data in 
+other class it will give the error 
+like "access_modify.model is inaaccessible" 
+the code is only accessible within the same class */
