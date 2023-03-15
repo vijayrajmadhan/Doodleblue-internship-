81d592abfbea3f4b2864f938ccebc1f7f5091a6a
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-15 15:03:30 +0530

    Create interface.cs
    another way to use the abstraction is interface

---
+++ Interface/interface.cs
@@ -0,0 +1,30 @@
+
+using System;
+interface solve
+{
+   void Area();
+}
+
+class valone:solve
+{
+    public void Area()
+    {
+        Console.WriteLine("the interface class is implimented");
+    }
+}
+
+class main
+{
+    static void Main(String[] args)
+    {
+        valone obj=new valone();
+        obj.Area();
+      
+    }
+}
+
+/* anotherway to achive abstraction is interface
+the member of interface is, abstract and public
+to use the interface it need to be inherited by the  other class
+no need to use override keyword when you implement interface 
+in abstract you need to use override key word */
