631134c106d9d3648244473265c67170ddb40c68
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-20 17:09:53 +0530

    Create for_loop.cs
    for loop will execute the value till the condition become false

---
+++ Loop/for_loop.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;//namespace
+
+class array
+{
+    public static void Main(string[] args)
+    {
+       //for loop
+       int n=5;
+       for(int i=0;i<n;i++) 
+       {
+           Console.WriteLine("Loop run till :"+i);
+       }
+    }
+}
+
+/* in for loop there are three statements statement1,statement2,statement3 all three statements are 
+work togather to run the for loop
+
+statement 1=>will asign the value
+statement 2=>condition check will happen the statement
+statement 3=>it will work to make the condition is wrong
+
+this three will work toughter to complete the loop*/
