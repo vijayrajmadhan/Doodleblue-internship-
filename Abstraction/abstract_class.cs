f4f97168e31987938660aff24b407880523bcbc2
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-03-22 22:25:55 +0530

    Create abstract_class.cs
    it is abstract class

---
+++ Abstraction/abstract_class.cs
@@ -0,0 +1,22 @@
+using System;
+abstract class solve
+{
+  public void Area()
+  {
+      Console.WriteLine("abstract class");
+  }
+}
+
+class valone:solve
+{
+   
+}
+
+class main
+{
+    static void Main(String[] args)
+    {
+        valone obj=new valone();
+        obj.Area();
+    }
+}
