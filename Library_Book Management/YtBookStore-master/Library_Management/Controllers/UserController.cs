66a5193098f1ad086b39df1a7bdf38e6379d2fcf
Vijay V <43164064+vijayrajmadhan@users.noreply.github.com>
2023-05-03 15:18:15 +0530

    ASP.Net Core Project

---
+++ Library_Book Management/YtBookStore-master/Library_Management/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YtBookStore.Models.Domain;
+using YtBookStore.Repositories.Abstract;
+
+namespace YtBookStore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IBookService bookService;
+        private readonly IAuthorService authorService;
+        private readonly IGenreService genreService;
+        private readonly IPublisherService publisherService;
+        public UserController(IBookService bookService, IGenreService genreService, IPublisherService publisherService, IAuthorService authorService)
+        {
+            this.bookService = bookService;
+            this.genreService = genreService;
+            this.publisherService = publisherService;
+            this.authorService = authorService;
+        }
+
+        public IActionResult GetAll()
+        {
+
+            var data = bookService.GetAll();
+            return View(data);
+        }
+
+        public async Task<IActionResult> Index(string SearchString)
+        {
+            ViewData["CurrentFilter"] = SearchString;
+            var books = from b in bookService.GetAll()
+                        select b;
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(b => b.Title.Contains(SearchString));
+            }
+            return View(books);
+        }
+    }
+}
